7ed394a37414308d711b946dcaff23e2b0b85562
Sacha Trémoureux <sacha@tremoureux.fr>
2014-12-09 15:24:42 +0100

    Add a submodule for org-mode and initialize it

--- init.el
+++ init.el
@@ -1,5 +1,12 @@
 ;;; init.el — Where all the magic begins
 (package-initialize nil)
+(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/lisp")
+(add-to-list 'load-path "~/.emacs.d/elisp/org-mode/contrib/lisp")
+;; Load the rest of the packages
+(package-initialize t)
+(setq package-enable-at-startup nil)
 
 (require 'org)
 (org-babel-load-file (concat (getenv "HOME") "/.emacs.d/sacha.org"))
+
+
