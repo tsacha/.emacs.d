fe73e687fcee1ee10613537c3ea053e34cf3b455
Sacha Trémoureux <sacha@tremoureux.fr>
2014-12-09 15:50:53 +0100

    Emacs initialization, use-package submodule, backups, history

--- init.el
+++ init.el
@@ -1,5 +1,6 @@
 ;;; init.el — Where all the magic begins
 (package-initialize nil)
+(add-to-list 'load-path "~/.emacs.d/elisp/use-package")
 (add-to-list 'load-path "~/.emacs.d/elisp/org-mode/lisp")
 (add-to-list 'load-path "~/.emacs.d/elisp/org-mode/contrib/lisp")
 ;; Load the rest of the packages
